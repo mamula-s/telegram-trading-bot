5ce567b00e1120a4528323b8f9acdc680006dfb4
mamula-s <sergijmamula@gmail.com>
2024-10-28 12:37:50 +0200

    fix: update webapp build configuration

--- src/webApp/App.jsx
+++ src/webApp/App.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import MainPage from './pages/MainPage';
+import FuturesPage from './pages/FuturesPage';
+import SpotPage from './pages/SpotPage';
+import EducationPage from './pages/EducationPage';
+import ReferralPage from './pages/ReferralPage';
 
 const App = () => {
-  console.log('App component rendering');
-  
   return (
-    <div className="p-4">
-      <div className="bg-blue-100 p-4 rounded-xl">
-        <h2 className="text-xl font-bold">Тестовий компонент</h2>
-        <p>Якщо ви бачите це, React працює правильно</p>
-      </div>
-    </div>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index element={<MainPage />} />
+        <Route path="futures" element={<FuturesPage />} />
+        <Route path="spot" element={<SpotPage />} />
+        <Route path="education" element={<EducationPage />} />
+        <Route path="referral" element={<ReferralPage />} />
+      </Route>
+    </Routes>
   );
 };
 
