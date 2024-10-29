25031fbf40ebc6ee89537ae586b421d1880d640b
mamula-s <sergijmamula@gmail.com>
2024-10-29 14:47:49 +0200

    fix: resolve routes duplication in App.jsx

--- src/webApp/App.jsx
+++ src/webApp/App.jsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { ApiProvider } from './contexts/ApiContext';
-import { NotificationProvider } from './contexts/NotificationContext';
-import Layout from './components/Layout';
 // src/webApp/App.jsx
+import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { WebSocketProvider } from './contexts/WebSocketContext';
-import Routes from './routes';
+import { NotificationProvider } from './contexts/NotificationContext';
+import { ApiProvider } from './contexts/ApiContext';
+import { Routes, Route } from 'react-router-dom';
 
 // Pages
 import MainPage from './pages/MainPage';
@@ -14,11 +12,9 @@ import FuturesPage from './pages/FuturesPage';
 import SpotPage from './pages/SpotPage';
 import EducationPage from './pages/EducationPage';
 import ReferralPage from './pages/ReferralPage';
-import AirdropsPage from './pages/AirdropsPage';
 import ChatsPage from './pages/ChatsPage';
 import PaymentPage from './pages/PaymentPage';
-
-
+import Layout from './components/Layout';
 
 const App = () => {
   return (
@@ -26,18 +22,17 @@ const App = () => {
       <NotificationProvider>
         <WebSocketProvider>
           <ApiProvider>
-          <Routes>
-          <Route element={<Layout />}>
-            <Route index element={<MainPage />} />
-            <Route path="futures" element={<FuturesPage />} />
-            <Route path="spot" element={<SpotPage />} />
-            <Route path="education" element={<EducationPage />} />
-            <Route path="referral" element={<ReferralPage />} />
-            <Route path="airdrops" element={<AirdropsPage />} />
-            <Route path="chats" element={<ChatsPage />} />
-            <Route path="payment" element={<PaymentPage />} />
-          </Route>
-        </Routes>
+            <Routes>
+              <Route element={<Layout />}>
+                <Route index element={<MainPage />} />
+                <Route path="futures" element={<FuturesPage />} />
+                <Route path="spot" element={<SpotPage />} />
+                <Route path="education" element={<EducationPage />} />
+                <Route path="referral" element={<ReferralPage />} />
+                <Route path="chats" element={<ChatsPage />} />
+                <Route path="payment" element={<PaymentPage />} />
+              </Route>
+            </Routes>
           </ApiProvider>
         </WebSocketProvider>
       </NotificationProvider>
